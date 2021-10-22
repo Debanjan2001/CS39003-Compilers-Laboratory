9e2607e1d96641be0a6a99d5adbfc897319c5128
Debanjan Saha <debanjansaha2001@gmail.com>
2021-10-22 21:16:04 +0530

    copy: finish minor edited copy-pasting

--- Assignment-5/ass5_19CS10048_19CS30014_translator.cxx
+++ Assignment-5/ass5_19CS10048_19CS30014_translator.cxx
@@ -353,12 +353,12 @@ void quad::print()
 
 void quad::type1() // Printing binary operators
 {
-    std::cout << res << " = " << arg1 << " " << op << " " << arg2;
+    cout << res << " = " << arg1 << " " << op << " " << arg2;
 }
 
 void quad::type2() // Printing relation operators and jumps
 {
-    std::cout << "if " << arg1 << " " << op << " " << arg2 << " goto " << res;
+    cout << "if " << arg1 << " " << op << " " << arg2 << " goto " << res;
 }
 
 //------------------------------------------------------//
@@ -375,38 +375,58 @@ void basicType::addType(string t, int s) // Add new trivial type to type Symbol 
 //--------------------------------------------------------------//
 void quadArray::print() // print the quad Array i.e the list of TAC
 {
-    for (int i = 0; i < 60; i++)
-        std::cout << "__";
-    std::cout << std::endl;
+    for (int lines = 0; lines < 55; ++lines)
+        cout << "--";
+    cout << endl;
 
-    std::cout << "THREE ADDRESS CODE (TAC): " << std::endl; // print all the three address codes TAC
-    for (int i = 0; i < 60; i++)
-        std::cout << "__";
-    std::cout << std::endl;
+    cout << "Three Address Code (TAC): " << endl; // print all the three address codes TAC
+    for (int lines = 0; lines < 55; ++lines)
+        cout << "--";
+    cout << endl;
 
-    int j = 0;
+    int counter = 0;
     vector<quad>::iterator it; // vector iterator to iterate through all the TAC in the array
-    it = Array.begin();
-    while (it != Array.end())
+    for (it = Array.begin(); it != Array.end(); ++it, counter++)
     {
         if (it->op == "label") // print the label if it is the operator
         {
-            std::cout << std::endl
-                      << j << ": ";
+            cout << endl;
+            cout << counter << ": ";
             it->print();
         }
         else
-        { // otherwise give 4 spaces and then print
-            std::cout << j << ": ";
+        {
+            // otherwise give 4 spaces and then print
+            cout << counter << ": ";
             generateSpaces(4);
             it->print();
         }
-        it++;
-        j++;
     }
-    for (int i = 0; i < 65; i++)
-        std::cout << "__"; // End of printing of the TAC
-    std::cout << std::endl;
+
+    // End TAC
+    for (int lines = 0; lines < 55; ++lines)
+        cout << "--";
+    cout << endl;
+}
+
+void generateSpaces(int n) // Generate required number of spaces
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << " ";
+    }
+}
+
+string convInt2String(int a) // helper function to convert int to string
+{
+    return to_string(a);
+}
+
+string convFloat2String(float x) // Take float as input and produce string as output
+{
+    std::ostringstream buff;
+    buff << x;
+    return buff.str();
 }
 
 //------------------------------------------------------------------//
@@ -416,20 +436,189 @@ void quadArray::print() // print the quad Array i.e the list of TAC
 //----------------- Emit a three address code TAC and add it to the Quad Array ------------
 void emit(string op, string res, string arg1, string arg2)
 {
-    quad *q1 = new quad(res, arg1, op, arg2);
-    Q.Array.push_back(*q1);
+    quad *myQuad = new quad(res, arg1, op, arg2);
+    Q.Array.push_back(*myQuad);
 }
 
 void emit(string op, string res, int arg1, string arg2)
 {
-    quad *q2 = new quad(res, arg1, op, arg2);
-    Q.Array.push_back(*q2);
+    quad *myQuad = new quad(res, arg1, op, arg2);
+    Q.Array.push_back(*myQuad);
 }
 
 void emit(string op, string res, float arg1, string arg2)
 {
-    quad *q3 = new quad(res, arg1, op, arg2);
-    Q.Array.push_back(*q3);
+    quad *myQuad = new quad(res, arg1, op, arg2);
+    Q.Array.push_back(*myQuad);
+}
+
+sym *convertType(sym *s, string rettype) // convert symbol s into the required return type
+{
+    sym *mySymbol = gentemp(new symboltype(rettype));
+    if (s->type->type == "char") // if type char
+    {
+        if (rettype == "int") // converting char to int
+        {
+            emit("=", mySymbol->name, "char2int(" + (*s).name + ")");
+            return mySymbol;
+        }
+        if (rettype == "double") // or converting to double
+        {
+            emit("=", mySymbol->name, "char2double(" + (*s).name + ")");
+            return mySymbol;
+        }
+        return s;
+    }
+    else if (s->type->type == "float") // if type float
+    {
+        if (rettype == "int") // converting float to int
+        {
+            emit("=", mySymbol->name, "float2int(" + (*s).name + ")");
+            return mySymbol;
+        }
+        else if (rettype == "char") // or converting to char
+        {
+            emit("=", mySymbol->name, "float2char(" + (*s).name + ")");
+            return mySymbol;
+        }
+        return s;
+    }
+    else if (s->type->type == "int") // if type int
+    {
+        if (rettype == "float") // converting int to float
+        {
+            emit("=", mySymbol->name, "int2float(" + (*s).name + ")");
+            return mySymbol;
+        }
+        else if (rettype == "char") // or converting to char
+        {
+            emit("=", mySymbol->name, "int2char(" + (*s).name + ")");
+            return mySymbol;
+        }
+        return s;
+    }
+    return s;
+}
+
+void changeTable(symtable *newtable) // Change current symbol table
+{
+    ST = newtable;
+}
+
+bool compareSymbolType(sym *&s1, sym *&s2) // Check if the symbols have same type or not
+{
+    symboltype *myType1 = s1->type; // get the basic type of symbol 1
+    symboltype *myType2 = s2->type; // get the basic type of symbol 2
+    bool checker = false;
+
+    if (compareSymbolType(myType1, myType2))
+        checker = true; // check if the two types are already equal
+    else if (s1 = convertType(s1, myType2->type))
+        checker = true; // check if one can be converted to the other then convert them
+    else if (s2 = convertType(s2, myType1->type))
+        checker = true; // check if one can be converted to the other then convert them
+
+    if (checker)
+        return true; // if the two types are compatible return true
+    else
+        return false; // else return false
+}
+
+bool compareSymbolType(symboltype *t1, symboltype *t2) // Check if the symbol types are same or not
+{
+    int checker = 0;
+    if (t1 == NULL && t2 == NULL)
+        checker = 1; // if both symbol types are NULL
+    else if (t1 == NULL || t2 == NULL || t1->type != t2->type)
+        checker = 2; // if only one of them is NULL or if base type isn't same
+
+    if (checker == 1)
+        return true;
+    else if (checker == 2)
+        return false;
+    else
+        return compareSymbolType(t1->arrtype, t2->arrtype); // otherwise check their Array type
+}
+
+//-------------------------------------------------------------//
+//            Backpatching and related functions               //
+//-------------------------------------------------------------//
+void backpatch(list<int> list1, int addr) // backpatching
+{
+    string myStr = convInt2String(addr); // get string form of the address
+
+    for (list<int>::iterator itr = list1.begin(); itr != list1.end(); ++itr)
+    {
+        Q.Array[*itr].res = myStr; // do the backpatching
+    }
+}
+
+list<int> makelist(int init)
+{
+    list<int> newlist(1, init); // make a new list
+    return newlist;             // return the newly created list
+}
+
+list<int> merge(list<int> &a, list<int> &b)
+{
+    a.merge(b); // merge two existing lists
+    return a;   // return the merged list
+}
+
+//----------------------------------------------------------------------//
+//          Other helper functions required for TAC generation          //
+//----------------------------------------------------------------------//
+
+//------------- Type checking and Type conversion functions -------------
+
+Expression *convBool2Int(Expression *e) // Convert any Expression to bool using standard procedure
+{
+    if (e->type == "bool")
+    {
+        e->loc = gentemp(new symboltype("int")); // use general goto statements and standard procedure
+        backpatch(e->truelist, nextinstr());
+        emit("=", e->loc->name, "true");
+        int nxt = nextinstr() + 1;
+        string myStr = convInt2String(nxt);
+        emit("goto", myStr);
+        backpatch(e->falselist, nextinstr());
+        emit("=", e->loc->name, "false");
+    }
+    return e;
+}
+
+Expression *convInt2Bool(Expression *e) // Convert any Expression to bool using standard procedure
+{
+    if (e->type != "bool")
+    {
+        e->falselist = makelist(nextinstr()); // update the falselist
+        emit("==", "", e->loc->name, "0");    // emit general goto statements
+        e->truelist = makelist(nextinstr());  // update the truelist
+        emit("goto", "");
+    }
+    return e;
+}
+
+int nextinstr()
+{
+    return Q.Array.size(); // next instruction will be 1+last index and lastindex=size-1. hence return size
+}
+
+void update_nextinstr()
+{
+    instr_count++;
+    if (debug_on == 1)
+    {
+        cout << "Current Line Number:" << instr_count << endl;
+        cout << "Press [ENTER] to continue:";
+        cin.get();
+    }
+}
+
+void debug()
+{
+    if (debug_on == 1)
+        cout << instr_count << endl;
 }
 
 /**
@@ -451,247 +640,91 @@ void emit(string op, string res, float arg1, string arg2)
  * table entry
  */
 sym *gentemp(symboltype *t, string str_new)
-{                                                        // generate temp variable
-    string tmp_name = "t" + convInt2String(ST->count++); // generate name of temporary variable
-    sym *s = new sym(tmp_name);
-    (*s).type = t;
-    (*s).size = computeSize(t); // calculate the size of the current symbol
-    (*s).val = str_new;
-    ST->table.push_back(*s); // push the newly created symbol in the Symbol table
+{                                                           // generate temp variable
+    string my_gen_name = "t" + convInt2String(ST->count++); // generate name of temporary variable
+    sym *mySymbol = new sym(my_gen_name);
+    mySymbol->type = t;
+    mySymbol->size = computeSize(t); // calculate the size of the current symbol
+    mySymbol->val = str_new;
+    ST->table.push_back(*mySymbol); // push the newly created symbol in the Symbol table
     return &ST->table.back();
 }
 
 label *find_label(string _name)
 {
-    for (vector<label>::iterator it = label_table.begin(); it != label_table.end(); it++)
+    vector<label>::iterator itr;
+    while (itr != label_table.end())
     {
-        if (it->name == _name)
-            return &(*it);
+        if (itr->name == _name)
+            return &(*itr);
+        ++itr;
     }
     return nullptr;
-}
-
-//-------------------------------------------------------------//
-//            Backpatching and related functions               //
-//-------------------------------------------------------------//
-void backpatch(list<int> list1, int addr) // backpatching
-{
-    string str = convInt2String(addr); // get string form of the address
-    list<int>::iterator it;
-    it = list1.begin();
-
-    while (it != list1.end())
-    {
-        Q.Array[*it].res = str; // do the backpatching
-        it++;
-    }
-}
-
-list<int> makelist(int init)
-{
-    list<int> newlist(1, init); // make a new list
-    return newlist;             // return the newly created list
-}
-
-list<int> merge(list<int> &a, list<int> &b)
-{
-    a.merge(b); // merge two existing lists
-    return a;   // return the merged list
-}
-
-//----------------------------------------------------------------------//
-//          Other helper functions required for TAC generation          //
-//----------------------------------------------------------------------//
-
-//------------- Type checking and Type conversion functions -------------
-string convInt2String(int a) // helper function to convert int to string
-{
-    return to_string(a);
-}
-
-string convFloat2String(float x) // Take float as input and produce string as output
-{
-    std::ostringstream buff;
-    buff << x;
-    return buff.str();
-}
-
-Expression *convBool2Int(Expression *e) // Convert any Expression to bool using standard procedure
-{
-    if (e->type == "bool")
-    {
-        e->loc = gentemp(new symboltype("int")); // use general goto statements and standard procedure
-        backpatch(e->truelist, nextinstr());
-        emit("=", e->loc->name, "true");
-        int p = nextinstr() + 1;
-        string str = convInt2String(p);
-        emit("goto", str);
-        backpatch(e->falselist, nextinstr());
-        emit("=", e->loc->name, "false");
-    }
-    return e;
-}
-
-Expression *convInt2Bool(Expression *e) // Convert any Expression to bool using standard procedure
-{
-    if (e->type != "bool")
-    {
-        e->falselist = makelist(nextinstr()); // update the falselist
-        emit("==", "", e->loc->name, "0");    // emit general goto statements
-        e->truelist = makelist(nextinstr());  // update the truelist
-        emit("goto", "");
-    }
-    return e;
-}
-
-sym *convertType(sym *s, string rettype) // convert symbol s into the required return type
-{
-    sym *temp = gentemp(new symboltype(rettype));
-    if ((*s).type->type == "float") // if type float
-    {
-        if (rettype == "int") // converting float to int
-        {
-            emit("=", temp->name, "float2int(" + (*s).name + ")");
-            return temp;
-        }
-        else if (rettype == "char") // or converting to char
-        {
-            emit("=", temp->name, "float2char(" + (*s).name + ")");
-            return temp;
-        }
-        return s;
-    }
-    else if ((*s).type->type == "int") // if type int
-    {
-        if (rettype == "float") // converting int to float
-        {
-            emit("=", temp->name, "int2float(" + (*s).name + ")");
-            return temp;
-        }
-        else if (rettype == "char") // or converting to char
-        {
-            emit("=", temp->name, "int2char(" + (*s).name + ")");
-            return temp;
-        }
-        return s;
-    }
-    else if ((*s).type->type == "char") // if type char
-    {
-        if (rettype == "int") // converting char to int
-        {
-            emit("=", temp->name, "char2int(" + (*s).name + ")");
-            return temp;
-        }
-        if (rettype == "double") // or converting to double
-        {
-            emit("=", temp->name, "char2double(" + (*s).name + ")");
-            return temp;
-        }
-        return s;
-    }
-    return s;
-}
-
-void changeTable(symtable *newtable) // Change current symbol table
-{
-    ST = newtable;
-}
-
-bool compareSymbolType(sym *&s1, sym *&s2) // Check if the symbols have same type or not
-{
-    symboltype *type1 = s1->type; // get the basic type of symbol 1
-    symboltype *type2 = s2->type; // get the basic type of symbol 2
-    int flag = 0;
-
-    if (compareSymbolType(type1, type2))
-        flag = 1; // check if the two types are already equal
-    else if (s1 = convertType(s1, type2->type))
-        flag = 1; // check if one can be converted to the other then convert them
-    else if (s2 = convertType(s2, type1->type))
-        flag = 1; // check if one can be converted to the other then convert them
-
-    if (flag)
-        return true; // if the two types are compatible return true
-    else
-        return false; // else return false
-}
-
-bool compareSymbolType(symboltype *t1, symboltype *t2) // Check if the symbol types are same or not
-{
-    int flag = 0;
-    if (t1 == NULL && t2 == NULL)
-        flag = 1; // if both symbol types are NULL
-    else if (t1 == NULL || t2 == NULL || t1->type != t2->type)
-        flag = 2; // if only one of them is NULL or if base type isn't same
-
-    if (flag == 1)
-        return true;
-    else if (flag == 2)
-        return false;
-    else
-        return compareSymbolType(t1->arrtype, t2->arrtype); // otherwise check their Array type
 }
 
 //----------------------------------------------------------------------//
 //           Other helper function for debugging and printing           //
 //----------------------------------------------------------------------//
 
-void generateSpaces(int n) // Generate required number of spaces
-{
-    while (n--)
-        std::cout << " ";
-}
-
-int nextinstr()
-{
-    return Q.Array.size(); // next instruction will be 1+last index and lastindex=size-1. hence return size
-}
-
 int computeSize(symboltype *t) // calculate size function
 {
+    if (t->type.compare("arr") == 0)
+        return t->width * computeSize(t->arrtype); // recursive for arrays (Multidimensional arrays)
+
     if (t->type.compare("void") == 0)
         return bt.size[1];
-    else if (t->type.compare("char") == 0)
+
+    if (t->type.compare("char") == 0)
         return bt.size[2];
-    else if (t->type.compare("int") == 0)
+
+    if (t->type.compare("int") == 0)
         return bt.size[3];
-    else if (t->type.compare("float") == 0)
+
+    if (t->type.compare("float") == 0)
         return bt.size[4];
-    else if (t->type.compare("ptr") == 0)
+
+    if (t->type.compare("ptr") == 0)
         return bt.size[5];
-    else if (t->type.compare("func") == 0)
+
+    if (t->type.compare("func") == 0)
         return bt.size[6];
-    else if (t->type.compare("arr") == 0)
-        return t->width * computeSize(t->arrtype); // recursive for arrays (Multidimensional arrays)
-    else
-        return -1;
+
+    return -1;
 }
 
 string printType(symboltype *t) // Print type of variable(imp for multidimensional arrays)
 {
     if (t == NULL)
         return bt.type[0];
+
     if (t->type.compare("void") == 0)
         return bt.type[1];
-    else if (t->type.compare("char") == 0)
+
+    if (t->type.compare("char") == 0)
         return bt.type[2];
-    else if (t->type.compare("int") == 0)
+
+    if (t->type.compare("int") == 0)
         return bt.type[3];
-    else if (t->type.compare("float") == 0)
+
+    if (t->type.compare("float") == 0)
         return bt.type[4];
-    else if (t->type.compare("ptr") == 0)
+
+    if (t->type.compare("ptr") == 0)
         return bt.type[5] + "(" + printType(t->arrtype) + ")"; // recursive for ptr
-    else if (t->type.compare("arr") == 0)
+
+    if (t->type.compare("arr") == 0)
     {
-        string str = convInt2String(t->width); // recursive for arrays
-        return bt.type[6] + "(" + str + "," + printType(t->arrtype) + ")";
+        string myStr = convInt2String(t->width); // recursive for arrays
+        return bt.type[6] + "(" + myStr + "," + printType(t->arrtype) + ")";
     }
-    else if (t->type.compare("func") == 0)
+
+    if (t->type.compare("func") == 0)
         return bt.type[7];
-    else if (t->type.compare("block") == 0)
+
+    if (t->type.compare("block") == 0)
         return bt.type[8];
-    else
-        return "NA";
+
+    return "NA";
 }
 
 int main()
@@ -707,12 +740,13 @@ int main()
     bt.addType("int", 4);
     bt.addType("float", 8);
     bt.addType("ptr", 4);
+    bt.addType("block", 0);
     bt.addType("arr", 0);
     bt.addType("func", 0);
-    bt.addType("block", 0);
 
     label_table.clear();
 
+    instr_count = 0;                   // count of instr (used for sanity check)
     table_count = 0;                   // count of nested table
     debug_on = 0;                      // debugging is off
     globalST = new symtable("Global"); // Global Symbol Table
@@ -722,7 +756,7 @@ int main()
 
     yyparse();          // initialize parse
     globalST->update(); // update the global Symbol Table
-    std::cout << "\n";
+    cout << "\n";
 
     Q.print();         // print the three address codes
     globalST->print(); // print all Symbol Tables
