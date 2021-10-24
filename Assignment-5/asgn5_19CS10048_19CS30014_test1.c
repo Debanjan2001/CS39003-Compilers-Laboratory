92939df4736a1df8fcd5b9a02b9a5d5960288309
“prit108” <pritgodhani132@gmail.com>
2021-10-24 21:27:27 +0530

    Some change in .y

--- Assignment-5/asgn5_19CS10048_19CS30014_test1.c
+++ Assignment-5/asgn5_19CS10048_19CS30014_test1.c
@@ -1,33 +1,32 @@
 //  arrays (multidimensional) ,  loops and nested loops
-int x = 0;
+int glb1 = 5;
 
 int main()
 {
-	int i, j, n;
-	int sum=0;
-	char a='a';
-	int p[5]; // 1D integer array
-	int dp[5][5]; // 2D integer array
-	n=5;
-	j=100;
-	i=0;
+	int i = 0, j = 0, n;
+	int sum = 0;
+	char bb = '#';
+	char esc = "\t";
+	int num[5]; // 1D integer array
+	int arr2d[25][25]; // 2D integer array
 
-	while(i<5) // while loop1
-	{
+	// do-while
+	do {
+		sum = sum + num[i];
+	} while(i<5);
+
+	// while
+	while(i<5) {
 		i++;
 		++j;
-		p[i]=i*j;
+		num[i] = i*j + 2*i + 3*j;
 	}
-
-	do // do-while loop
-	{
-		sum = sum + p[i];
-	}while(i<n);
-
-	for(i=0;i<n;i++)
-	{
+	
+	n = glb1 * glb1;
+	for(i=0;i<n;i++) {
 		for(j=0;j<n;j++)  // nested for loop
-			dp[i][j]= sum + i*j; // multi dimensional array
+			arr2d[i][j] = i*j; // multi dimensional array
 	}
+
 	return 0;
 }
